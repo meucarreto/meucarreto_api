3ad280de7962268f82b266c2135f987634c27b1f
Carlos C. Matos <carlos@multifarmas.com.br>
2017-01-03 15:59:45 -0200

    Ajuste caminho de app.js

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,6 +2,6 @@
 cd /var/www/html/meucarreto_api
 npm install --silent
 chown -R ec2-user:apache /var/www/html/meucarreto_api/
-pm2 start sever/server.js --name back
+pm2 start server/server.js --name back
 pm2 start all
 exit 0
